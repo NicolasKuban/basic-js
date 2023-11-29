8847ba40246dd6ac0e17c784885029551e535675
NicolasKuban <nicrad@yandex.ru>
2023-11-29 18:55:17 +0300

    feat: Реализована функция carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // t = (1 / lambda) * Math.LN2(1 + D / P) 
+  if (arguments.length === 0) {return false}
+  // console.log(typeof sampleActivity)
+  if (typeof sampleActivity !== "string") {return false};
+  let activity = Number(sampleActivity);
+  // console.log(activity);
+  if (isNaN(activity)) {return false}
+  if (activity <= 0 || activity > MODERN_ACTIVITY) {return false}
+  // ff = (HALF_LIFE_PERIOD / Math.log(2))
+  // ss = Math.log(activity / MODERN_ACTIVITY)
+  // console.log(ff, ss)
+  return Math.ceil((HALF_LIFE_PERIOD / Math.log(2)) * (-Math.log(activity / MODERN_ACTIVITY)))
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+
+
+a = "15.1"
+console.log(dateSample(a))
 
 module.exports = {
   dateSample
