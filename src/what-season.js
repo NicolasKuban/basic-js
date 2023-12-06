67057d29a49c8041c8f05262a6a96bb6a5a6b9d2
NicolasKuban <nicrad@yandex.ru>
2023-12-07 02:15:58 +0300

    feat: Осталось решить 4 задачи

--- src/what-season.js
+++ src/what-season.js
@@ -18,10 +18,28 @@ function getSeason(date) {
     // console.log(date, "==", month)
   }
   // console.log(new Date(date), String(new Date(date)) === "Invalid Date")
-  if(String(new Date(date)) === "Invalid Date") {
-    // throw new Error("Invalid Date");
+  // console.log((typeof date === "object"))
+  // console.log(String(new Date(date)) === "Invalid Date")
+  if((String(new Date(date)) === "Invalid Date") || (typeof date !== "object")) {
+    // console.log("==ERROR==")
+    throw new Error("Invalid date!");
   }
-  month = (date.getMonth() + 1) % 12
+  // console.log("have JSON", (new Date).toJSON, date.toJSON, (new Date).getDay !== date.getDay )
+  if ((new Date).getDay !== date.getDay) {
+    throw new Error("Invalid date!");
+  }
+  try {
+    month = (date.getMonth() + 1) % 12  
+  } catch (e) {
+    // console.log("==ERROR==", e.name)
+    if (e.name == "TypeError") {
+      throw new Error("Invalid date!");
+    } else {
+      throw e;
+    }
+  }
+  // month = (date.getMonth() + 1) % 12
+
   if (month <3) {return "winter"}
   if (month <6) {return "spring"}
   if (month <9) {return "summer"}
@@ -50,13 +68,66 @@ function getSeason(date) {
 // console.log(getSeason(summer)) //, 'summer');
 // console.log(getSeason(autumn)) //.to.match(/autumn|fall/);
 
-console.log(getSeason("fdo"))
+// console.log(getSeason("fdo"))
+// console.log(getSeason('foo'))
+// console.log(getSeason({ John: 'Smith' }))
 
 
+// console.log(getSeason(20192701))
+// console.log(getSeason([2019, '27', 0 + '1']))
+// console.log(getSeason(() => new Date()))
 
 
+// const fakeDate = {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+
+// console.log(getSeason(fakeDate))
+//], 'Invalid date!');
 
 
+// const deeperFakeDate = {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   getMonth() {
+//       return Date.prototype.getMonth.call(new Date());
+//   },
+//   getFullYear() {
+//       return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+//   },
+//   getDate() {
+//       return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+//   },
+//   getHours() {
+//       return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+//   },
+//   getMinutes() {
+//       return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+//   },
+//   getSeconds() {
+//       return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+//   },
+//   getMilliseconds() {
+//       return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+//   },
+//   getDay() {
+//       return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// Object.setPrototypeOf(deeperFakeDate, Object.getPrototypeOf(new Date()));
+
+// console.log(getSeason(deeperFakeDate))
+//], 'Invalid date!');
+
+// Object.prototype.hasOwnProperty("toJSON")
 module.exports = {
   getSeason
 };
